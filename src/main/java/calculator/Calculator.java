d5aca6c3c6097085a98857c6d0ad63d7f0b71c53
jkh1447 <jkh1447@gmail.com>
2025-10-15 17:37:31 +0900

    feat(parseString): 문자열에서 커스텀구분자와 숫자부분을 분리
    문자열에서 커스텀 구분자와 숫자부분을 분리하고, 잘못된 입력에 대하여
    예외처리함.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,8 +9,50 @@ public class Calculator {
     public void calculate() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = readLine();
+        int result = parseAndSum(str);
 
     }
 
+    public int parseAndSum(String str) {
+        if(str == null) {
+            throw new IllegalArgumentException("string is null");
+        }
 
+        if(str.isEmpty()) {
+            return 0;
+        }
+
+        if(str.startsWith("//")) {
+            // "//"로 시작하는 경우
+            int idx = str.indexOf("\\n");
+            if (idx == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 설정은 \"//\" 로 시작하여 \"\\n\"로 끝나야 합니다.");
+            }
+            String delimiter = Pattern.quote(str.substring(2, idx));
+            if(delimiter.isEmpty()) {
+                throw new IllegalArgumentException("커스텀 구분자가 비어있습니다.");
+            }
+            if(idx == str.length()-1) {
+                throw new IllegalArgumentException("계산할 숫자가 비어있습니다.");
+            }
+            String num = str.substring(idx + 2);
+
+            return sum(delimiter, num);
+        }
+        else if ('0' <= str.charAt(0) &&  str.charAt(0) <= '9') {
+            // 숫자로 시작하는 경우
+            String delimiter = ",|:";
+            return sum(delimiter, str);
+        }
+
+        throw new IllegalArgumentException("숫자나 \"//\"로 시작해야 합니다.");
+    }
+
+    public int sum(String delimiter, String nums){
+
+        int sum = 0;
+
+
+        return sum;
+    }
 }
