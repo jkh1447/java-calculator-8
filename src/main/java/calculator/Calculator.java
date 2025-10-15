133ef08da1486064058ee658ea72d14562562403
jkh1447 <jkh1447@gmail.com>
2025-10-15 17:26:11 +0900

    feat(input): 사용자의 입력 받기
    계산할 문자열을 사용자로부터 입력받아 저장함.

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Calculator {
+    public void calculate() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String str = readLine();
+
+    }
+
+
+}
